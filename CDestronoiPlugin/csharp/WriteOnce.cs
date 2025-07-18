9fb18408b3d3db0d1d85416b958f05e5121b773c
BenBlueeeee <benjaminleogreen@gmail.com>
2025-07-18 11:05:31 +0100

    feat: added WriteOnce.cs class
    used for permanentLeft and permanentRight references in the VSTNode class (as these have to be set after initialisation, specifically in the ready of DestronoiNode.cs (in Bisect), but they should only be set once)

---
+++ CDestronoiPlugin/csharp/WriteOnce.cs
@@ -0,0 +1,26 @@
+using Godot;
+
+namespace CDestronoi;
+
+public class WriteOnce<T>
+{
+	private T _value;
+	private bool isSet = false;
+
+	public T Value
+	{
+		get => _value;
+		set
+		{
+			if (isSet)
+            {
+                GD.PushWarning($"this variable {typeof(T).Name} cannot be set to twice. returning early");
+                return;
+            }
+			_value = value;
+			isSet = true;
+		}
+	}
+
+	public static implicit operator T(WriteOnce<T> writeOnce) => writeOnce._value;
+}
